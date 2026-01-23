17071ae387c1f5fa014089f544bc3b8b558007db
Naitik-Mishra-101 <naitik.2431397@gmail.com>
2026-01-23 12:18:42 +0530

    add file StackAndHeap.js

--- JAVASCRIPT/StackAndHeapM.js
+++ JAVASCRIPT/StackAndHeapM.js
@@ -2,7 +2,7 @@
 // heap(non-primitive)--> reference milta hai
 
 let my_name="Naitik";// let ko redeclare nhi kr sakte
-ghr=my_name;
+ghr=my_name;//ghr me my name ki copy mili hai original(reference) nahi
 ghr="Divyansh";
 console.log(my_name);//Naitik
 console.log(ghr);//Divyansh

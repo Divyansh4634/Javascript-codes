c91c1243171755e1433157dc73020f43a00f5e3f
Naitik-Mishra-101 <naitik.2431397@gmail.com>
2026-01-23 12:11:35 +0530

    add file LetOrVar.js

---
+++ JAVASCRIPT/LetOrVar.js
@@ -0,0 +1,12 @@
+// let:
+
+// let cat="billi";
+// let cat="meow";//without let--> it updates(Let me same name ka variable without let ke update hota hai)
+// console.log(cat);
+
+
+// var:
+
+// var cat="billi";
+// var cat="meow";
+// console.log(cat);

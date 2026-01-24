d85e9bd4d295a62550f79c573f147ed6b517567d
Naitik-Mishra-101 <naitik.2431397@gmail.com>
2026-01-24 11:58:35 +0530

    Add or apply changes

--- JAVASCRIPT/StackAndHeapM.js
+++ JAVASCRIPT/StackAndHeapM.js
@@ -1,5 +1,5 @@
-// stack(primitive) -->copy milti hai
-// heap(non-primitive)--> reference milta hai
+// stack(primitive) -->copy milti hai:stack me copy badalne se original nhi badlta hai.
+// heap(non-primitive)--> reference milta hai:heap me copy badalne se original badlta hai.
 
 let my_name="Naitik";// let ko redeclare nhi kr sakte
 ghr=my_name;//ghr me my name ki copy mili hai original(reference) nahi
---
+++ JAVASCRIPT/Strings.js
@@ -0,0 +1,37 @@
+//string writes either in double (" ") or in single quotes(' ') or via back-ticks(` `)
+// const First_name="Naitik";
+// const Last_Name="Mishra";
+// console.log(First_name+Last_Name);
+// console.log(`hello my name is ${First_name} and my sur_name is ${Last_Name}.`)// this method is best one.
+// console.log(First_name.slice(-6,3));
+// console.log(First_name.length);//length is not a function it is a property
+// console.log(First_name.toUpperCase());//NAITIK
+// console.log(First_name.charAt(4));//i
+// console.log(First_name.indexOf('i'));//2
+// console.log(First_name.substring(0,2));//end nhi aayega
+const newstring="                  Naitik-Mishra-ji     ";
+// console.log(newstring.trim());// trim works on "      " White-spaces;
+// console.log(newstring.replace('-','%20'));//phela - badlega saare - hatane hai to( /-/g )console.log(newstring.replace(/-/g, '%20'));
+// console.log(newstring.includes("j"));//hai ki nahiiii
+// console.log(newstring.split('-'));//- poori string seperate hoti hai
+console.log(newstring.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

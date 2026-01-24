97b781bbfbe22a8479f8bc7716f9de7c2ca4202f
Naitik-Mishra-101 <naitik.2431397@gmail.com>
2026-01-24 15:16:07 +0530

    add my Maths File

---
+++ JAVASCRIPT/Math.js
@@ -0,0 +1,27 @@
+const num=400;//number
+// console.log(num);
+
+// const numO=new Number (300)//object
+// console.log(numO);
+// console.log(num.toString().length);//3
+// console.log(typeof (num));//originalk ka type number hai
+// console.log(num.toFixed(2));//especially for e commerce Application it gives Fixed number of Decimal values 
+// console.log(num.toPrecision(1));//4e+2-->4*10^2
+// const hundereds=100000000000;
+// console.log(hundereds.toLocaleString("en-in"));//default--> international values; "en-in"-->english language- indian value
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++Maths+++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Math is apne aap me ek object
+
+// console.log(Math);
+// console.log(Math.abs(-4));//negative to positive
+// console.log(Math.round(4.6));//Widely Used
+// console.log(Math.ceil(4.01));//more than value(5) if it more as(.01)
+// console.log(Math.floor(4.99));//lesser value(4) if it is 0.99
+// console.log(Math.floor(Math.random()*10)+1);//value change in every run
+// const min=10;
+// const max=20;
+// console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
